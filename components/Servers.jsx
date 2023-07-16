7c8d61e2147c862cb038febc92e2898386029726
ismail safha <ismailsafha@gmail.com>
2023-07-16 19:15:19 +0100

    footer

---
+++ components/Servers.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Servers = () => {
+  return (
+    <section>
+      <div className="">
+        <i className="ri-customer-service-line"></i>
+      </div>
+      <div className=""></div>
+      <div className="">
+        <i className="ri-arrow-left-right-fill"></i>
+      </div>
+    </section>
+  );
+};
+
+export default Servers;
